82729c0415f2eabe6772d70dd205c561fab00724
Tania Batieva <yalyna.ts@gmail.com>
2020-05-19 18:41:13 +0300

    Change parameter type in parse_bool

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
 #[allow(dead_code)]
+use indexmap::IndexMap;
+use std::iter::Peekable;
+use std::path::Iter;
+use std::str::Chars;
+
+type Parameters = IndexMap<String, BareItem>;
+
+#[derive(Debug)]
+struct Item {
+    bare_item: BareItem,
+    parameters: Option<Parameters>,
+}
+
 #[derive(Debug, PartialEq)]
-enum Item {
+enum BareItem {
     Decimal(i64),
     Integer(i64),
     String(String),
@@ -11,28 +24,44 @@ enum Item {
 
 #[derive(Debug)]
 struct Parser {
-    input: String,
+    input_str: String,
 }
 
 impl Parser {
-    fn parse_bare_item(input: &str) -> Result<Item, ()> {
-        if input.starts_with('?') {
-            Ok(Item::Boolean(Parser::parse_bool(input)?.0))
-        } else {
-            return Err(());
+    // fn new(input_string: String) -> Self {
+    //     Parser {
+    //         input_str: input_string,
+    //     }
+    // }
+
+    // fn parse_item(self) -> Result<Item, ()> {
+    //     // parse item
+    //     // parse parameters
+    //     // return Item { ... }
+    //     let iter = self.input_str.chars().peekable();
+    //     let bare_item = Self::parse_bare_item(iter)?;
+    //     Ok(Item {
+    //         bare_item,
+    //         parameters: None,
+    //     })
+    // }
+
+    fn parse_bare_item(input: &mut Chars) -> Result<BareItem, ()> {
+        let mut x = input.clone();
+        match input.peekable().peek() {
+            Some(&'?') => Ok(BareItem::Boolean(Self::parse_bool(&mut x)?)),
+            _ => Err(()),
         }
     }
 
-    fn parse_bool(input: &str) -> Result<(bool, &str), ()> {
-        let mut iter = input.chars();
-
-        if iter.next() != Some('?') {
+    fn parse_bool(input: &mut Chars) -> Result<bool, ()> {
+        if input.next() != Some('?') {
             return Err(());
         }
 
-        match iter.next() {
-            Some('0') => Ok((false, &input["?0".len()..])),
-            Some('1') => Ok((true, &input["?0".len()..])),
+        match input.next() {
+            Some('0') => Ok(false),
+            Some('1') => Ok(true),
             _ => Err(()),
         }
     }
@@ -48,15 +77,17 @@ mod tests {
 
     #[test]
     fn parse_bare_item() {
-        assert_eq!(Item::Boolean(false), Parser::parse_bare_item("?0").unwrap());
+        assert_eq!(
+            Ok(BareItem::Boolean(false)),
+            Parser::parse_bare_item(&mut "?0".chars())
+        );
     }
-
     #[test]
     fn parse_bool() {
-        assert_eq!((false, ""), Parser::parse_bool("?0").unwrap());
-        assert_eq!((true, ""), Parser::parse_bool("?1").unwrap());
-        assert_eq!((false, "gk"), Parser::parse_bool("?0gk").unwrap());
-        assert_eq!(Err(()), Parser::parse_bool(""));
-        assert_eq!(Err(()), Parser::parse_bool("?"));
+        assert_eq!(false, Parser::parse_bool(&mut "?0".chars()).unwrap());
+        assert_eq!(true, Parser::parse_bool(&mut "?1".chars()).unwrap());
+        assert_eq!(false, Parser::parse_bool(&mut "?0gk".chars()).unwrap());
+        assert_eq!(Err(()), Parser::parse_bool(&mut "".chars()));
+        assert_eq!(Err(()), Parser::parse_bool(&mut "?".chars()));
     }
 }
