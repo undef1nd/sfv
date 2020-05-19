9f960624eeecb132ab4a3774137c5fde49ce925f
Tania Batieva <yalyna.ts@gmail.com>
2020-05-19 20:15:32 +0300

    Add parse_string

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[allow(dead_code)]
 use indexmap::IndexMap;
+use std::fs::read;
 use std::iter::Peekable;
 use std::path::Iter;
 use std::str::Chars;
@@ -46,10 +47,10 @@ impl Parser {
     //     })
     // }
 
-    fn parse_bare_item(input: &mut Chars) -> Result<BareItem, ()> {
-        let mut x = input.clone();
-        match input.peekable().peek() {
-            Some(&'?') => Ok(BareItem::Boolean(Self::parse_bool(&mut x)?)),
+    fn parse_bare_item(mut input: &mut Chars) -> Result<BareItem, ()> {
+        match input.clone().peekable().peek() {
+            Some(&'?') => Ok(BareItem::Boolean(Self::parse_bool(&mut input)?)),
+            Some(&'"') => Ok(BareItem::String(Self::parse_string(&mut input)?)),
             _ => Err(()),
         }
     }
@@ -64,6 +65,34 @@ impl Parser {
             Some('1') => Ok(true),
             _ => Err(()),
         }
+    }
+
+    fn parse_string(input: &mut Chars) -> Result<String, ()> {
+        if input.next() != Some('\"') {
+            return Err(());
+        }
+
+        let mut output_string = String::from("");
+        while let Some(curr_char) = input.next() {
+            if curr_char == '\\' {
+                if input.count() == 0 {
+                    return Err(());
+                }
+                if let Some(curr_char) = input.next() {
+                    if curr_char != '\\' && curr_char != '\"' {
+                        return Err(());
+                    }
+                    output_string.push(curr_char)
+                }
+            } else if curr_char == '\"' {
+                return Ok(output_string);
+            } else if (curr_char >= '\x00' && curr_char <= '\x1f') || curr_char == '\x7f' {
+                return Err(());
+            } else {
+                output_string.push(curr_char);
+            }
+        }
+        Err(())
     }
 
     fn parse_parameters() -> Result<(), ()> {
@@ -81,6 +110,10 @@ mod tests {
             Ok(BareItem::Boolean(false)),
             Parser::parse_bare_item(&mut "?0".chars())
         );
+        assert_eq!(
+            Ok(BareItem::String("test string".to_owned())),
+            Parser::parse_bare_item(&mut "\"test string\"".chars())
+        );
     }
     #[test]
     fn parse_bool() {
@@ -89,5 +122,19 @@ mod tests {
         assert_eq!(false, Parser::parse_bool(&mut "?0gk".chars()).unwrap());
         assert_eq!(Err(()), Parser::parse_bool(&mut "".chars()));
         assert_eq!(Err(()), Parser::parse_bool(&mut "?".chars()));
+    }
+
+    #[test]
+    fn parse_string() {
+        assert_eq!(
+            "test".to_owned(),
+            Parser::parse_string(&mut "\"test\"".chars()).unwrap()
+        );
+        assert_eq!(
+            "".to_owned(),
+            Parser::parse_string(&mut "\"\"".chars()).unwrap()
+        );
+        assert_eq!(Err(()), Parser::parse_string(&mut "\"\\".chars()));
+        assert_eq!(Err(()), Parser::parse_string(&mut "\"\\l\"".chars()));
     }
 }
