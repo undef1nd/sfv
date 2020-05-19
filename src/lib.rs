69442dc0229c19accf4c338a10c0d76f4047450f
Tania Batieva <yalyna.ts@gmail.com>
2020-05-19 18:24:21 +0300

    Change input type to char

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ impl Parser {
         // parse item
         // parse parameters
         // return Item { ... }
-        let iter = self.input_str.chars().peekable();
+        let iter = self.input_str.chars();
         let bare_item = Self::parse_bare_item(iter)?;
         Ok(Item {
             bare_item,
@@ -46,14 +46,14 @@ impl Parser {
         })
     }
 
-    fn parse_bare_item(mut input: Peekable<Chars>) -> Result<BareItem, ()> {
-        match input.peek() {
+    fn parse_bare_item(mut input: Chars) -> Result<BareItem, ()> {
+        match input.clone().peekable().peek() {
             Some(&'?') => Ok(BareItem::Boolean(Self::parse_bool(input)?.0)),
             _ => Err(()),
         }
     }
 
-    fn parse_bool(mut input: Peekable<Chars>) -> Result<(bool, Peekable<Chars>), ()> {
+    fn parse_bool(mut input: Chars) -> Result<(bool, Chars), ()> {
         if input.next() != Some('?') {
             return Err(());
         }
@@ -65,14 +65,16 @@ impl Parser {
         }
     }
 
-    fn parse_parameters() -> Result<(), ()> {
-        unimplemented!()
-    }
+    // fn parse_parameters() -> Result<(), ()> {
+    //     unimplemented!()
+    // }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
     // #[test]
     // fn parse_bare_item() {
@@ -81,17 +83,29 @@ mod tests {
     //
     #[test]
     fn parse_bool() {
-        let res = Parser::parse_bool("?0".chars().peekable()).unwrap();
-        assert_eq!(
-            (false, "".to_owned()),
-            (res.0, res.1.collect())
-        );
-        // assert_eq!(
-        //     (true, "".chars().peekable()),
-        //     Parser::parse_bool("?1".chars().peekable()).unwrap()
-        // );
-        // assert_eq!((false, "gk"), Parser::parse_bool("?0gk").unwrap());
-        // assert_eq!(Err(()), Parser::parse_bool(""));
-        // assert_eq!(Err(()), Parser::parse_bool("?"));
+        let mut test_data = HashMap::new();
+        test_data.insert("?0", (false, ""));
+        test_data.insert("?1", (true, ""));
+        test_data.insert("?1gl", (true, "gl"));
+
+
+        for (input, expected_output) in test_data {
+            let actual_output = Parser::parse_bool(input.chars());
+            let (actual_bool, actual_iter) = actual_output.unwrap();
+            let actual_iter = actual_iter.as_str();
+            assert_eq!(expected_output, (actual_bool, actual_iter));
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_bool_invalid() {
+        Parser::parse_bool("?".chars()).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_bool_empty_str() {
+        Parser::parse_bool("".chars()).unwrap();
     }
 }
