856cce0ff7a30b8021b02b28230c0fd4e4356cde
Tania Batieva <yalyna.ts@gmail.com>
2020-05-20 11:21:25 +0300

    Add parse_token func

--- src/lib.rs
+++ src/lib.rs
@@ -39,8 +39,8 @@ impl Parser {
     //     // parse item
     //     // parse parameters
     //     // return Item { ... }
-    //     let iter = self.input_str.chars().peekable();
-    //     let bare_item = Self::parse_bare_item(iter)?;
+    //     let chars_iter = self.input_str.chars().peekable();
+    //     let bare_item = Self::parse_bare_item(chars_iter)?;
     //     Ok(Item {
     //         bare_item,
     //         parameters: None,
@@ -51,6 +51,10 @@ impl Parser {
         match input.peek() {
             Some(&'?') => Ok(BareItem::Boolean(Self::parse_bool(&mut input)?)),
             Some(&'"') => Ok(BareItem::String(Self::parse_string(&mut input)?)),
+            // Some(&'*') => Ok(BareItem::Token(Self::parse_token(&mut input)?)),
+            Some(&c) if c == '*' || c.is_ascii_alphabetic() => {
+                Ok(BareItem::Token(Self::parse_token(&mut input)?))
+            }
             _ => Err(()),
         }
     }
@@ -76,10 +80,10 @@ impl Parser {
         while let Some(curr_char) = input.next() {
             if curr_char == '\\' {
                 match input.next() {
-                    Some('\\') =>  { output_string.push(curr_char) },
-                    Some ('\"') => { output_string.push(curr_char) },
-                    None => { return Err(()) }
-                    _ => { return Err(()) }
+                    Some('\\') => output_string.push(curr_char),
+                    Some('\"') => output_string.push(curr_char),
+                    None => return Err(()),
+                    _ => return Err(()),
                 }
             } else if (curr_char >= '\x00' && curr_char <= '\x1f') || curr_char == '\x7f' {
                 return Err(());
@@ -90,6 +94,31 @@ impl Parser {
             }
         }
         Err(())
+    }
+
+    fn parse_token(input: &mut Peekable<Chars>) -> Result<String, ()> {
+        if let Some(first_char) = input.peek() {
+            if !first_char.is_ascii_alphabetic() && first_char != &'*' {
+                return Err(());
+            }
+        } else {
+            return Err(());
+        }
+
+        let mut output_string = String::from("");
+        while let Some(curr_char) = input.peek() {
+            if !":/!#$%&'*+-.^_`|~".contains(curr_char.to_owned())
+                && !curr_char.is_ascii_alphanumeric()
+            {
+                return Ok(output_string);
+            }
+
+            match input.next() {
+                Some(c) => output_string.push(c),
+                None => return Err(()),
+            }
+        }
+        Ok(output_string)
     }
 
     fn parse_parameters() -> Result<(), ()> {
@@ -114,9 +143,18 @@ mod tests {
     }
     #[test]
     fn parse_bool() {
-        assert_eq!(false, Parser::parse_bool(&mut "?0".chars().peekable()).unwrap());
-        assert_eq!(true, Parser::parse_bool(&mut "?1".chars().peekable()).unwrap());
-        assert_eq!(false, Parser::parse_bool(&mut "?0gk".chars().peekable()).unwrap());
+        assert_eq!(
+            false,
+            Parser::parse_bool(&mut "?0".chars().peekable()).unwrap()
+        );
+        assert_eq!(
+            true,
+            Parser::parse_bool(&mut "?1".chars().peekable()).unwrap()
+        );
+        assert_eq!(
+            false,
+            Parser::parse_bool(&mut "?0gk".chars().peekable()).unwrap()
+        );
         assert_eq!(Err(()), Parser::parse_bool(&mut "".chars().peekable()));
         assert_eq!(Err(()), Parser::parse_bool(&mut "?".chars().peekable()));
     }
@@ -131,10 +169,33 @@ mod tests {
             "".to_owned(),
             Parser::parse_string(&mut "\"\"".chars().peekable()).unwrap()
         );
-        assert_eq!(Err(()), Parser::parse_string(&mut "test".chars().peekable()));
-        assert_eq!(Err(()), Parser::parse_string(&mut "\"\\".chars().peekable()));
-        assert_eq!(Err(()), Parser::parse_string(&mut "\"\\l\"".chars().peekable()));
-        assert_eq!(Err(()), Parser::parse_string(&mut "\"\u{1f}\"".chars().peekable()));
-        assert_eq!(Err(()), Parser::parse_string(&mut "\"smth".chars().peekable()));
+        assert_eq!(
+            Err(()),
+            Parser::parse_string(&mut "test".chars().peekable())
+        );
+        assert_eq!(
+            Err(()),
+            Parser::parse_string(&mut "\"\\".chars().peekable())
+        );
+        assert_eq!(
+            Err(()),
+            Parser::parse_string(&mut "\"\\l\"".chars().peekable())
+        );
+        assert_eq!(
+            Err(()),
+            Parser::parse_string(&mut "\"\u{1f}\"".chars().peekable())
+        );
+        assert_eq!(
+            Err(()),
+            Parser::parse_string(&mut "\"smth".chars().peekable())
+        );
+    }
+
+    #[test]
+    fn parse_token() {
+        assert_eq!(
+            "smth".to_owned(),
+            Parser::parse_token(&mut "smth".chars().peekable()).unwrap()
+        )
     }
 }
