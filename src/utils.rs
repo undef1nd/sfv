6cf077e7f64eff61b486360377dc1802f4fc801c
Tania Batieva <yalyna.ts@gmail.com>
2020-05-26 13:47:25 +0300

    Add helper to remove multiple spaces and tabs

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,28 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
 pub fn is_tchar(c: char) -> bool {
     // See tchar values list in https://tools.ietf.org/html/rfc7230#section-3.2.6
     let tchars = "!#$%&'*+-.^_'|~";
     tchars.contains(c) || c.is_ascii_alphanumeric()
+}
+
+pub fn consume_ows_chars(input_chars: &mut Peekable<Chars>) {
+    while let Some(c) = input_chars.peek() {
+        if c == &' ' || c == &'\t' {
+            input_chars.next();
+        } else {
+            break;
+        }
+    }
+}
+
+pub fn consume_sp_chars(input_chars: &mut Peekable<Chars>) {
+    while let Some(c) = input_chars.peek() {
+        if c == &' ' {
+            input_chars.next();
+        } else {
+            break;
+        }
+    }
 }
