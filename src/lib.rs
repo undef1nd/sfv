f96d60a8db5d66597ceae264f6ee2005c9d585a5
Tania Batieva <yalyna.ts@gmail.com>
2020-06-18 19:59:38 +0300

    Move tests into separate modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,65 @@
+mod utils;
+use indexmap::IndexMap;
+pub use rust_decimal::Decimal;
+
 pub mod parser;
 pub mod serializer;
-mod utils;
+pub(crate) mod test_parser;
+pub(crate) mod test_serializer;
 
 type Res<R> = Result<R, &'static str>;
+
+pub type Dictionary = IndexMap<String, ListEntry>;
+pub type Parameters = IndexMap<String, BareItem>;
+pub type List = Vec<ListEntry>;
+
+#[derive(Debug, PartialEq)]
+pub enum ListEntry {
+    Item(Item),
+    InnerList(InnerList),
+}
+
+impl From<Item> for ListEntry {
+    fn from(item: Item) -> Self {
+        ListEntry::Item(item)
+    }
+}
+
+impl From<InnerList> for ListEntry {
+    fn from(item: InnerList) -> Self {
+        ListEntry::InnerList(item)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct InnerList(pub Vec<Item>, pub Parameters);
+
+#[derive(Debug, PartialEq)]
+pub struct Item(pub BareItem, pub Parameters);
+
+#[derive(Debug, PartialEq)]
+pub enum Num {
+    Decimal(Decimal),
+    Integer(i64),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BareItem {
+    Number(Num),
+    String(String),
+    ByteSeq(Vec<u8>),
+    Boolean(bool),
+    Token(String),
+}
+
+impl From<i64> for BareItem {
+    fn from(item: i64) -> Self {
+        BareItem::Number(Num::Integer(item))
+    }
+}
+
+impl From<Decimal> for BareItem {
+    fn from(item: Decimal) -> Self {
+        BareItem::Number(Num::Decimal(item))
+    }
+}
