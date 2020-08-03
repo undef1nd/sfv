62c62f012e49db96d7fd9761b56025bab627059e
Ana Hobden <operator@hoverbear.org>
2020-08-03 06:21:37 -0700

    Use criterion for benching (#72)
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,49 +1,87 @@
 #[macro_use]
-extern crate bencher;
+extern crate criterion;
 
-use bencher::Bencher;
+use criterion::{BenchmarkId, Criterion};
 use sfv::{Parser, SerializeValue};
 
-fn parsing_item(bench: &mut Bencher) {
-    let input = "c29tZXZlcnlsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXNhbnNvbWVvdGhlcmxvbmdsaW5l";
-    bench.iter(|| Parser::parse_item(input.as_bytes()).unwrap());
+criterion_main!(parsing, serializing);
+
+criterion_group!(parsing, parsing_item, parsing_list, parsing_dict);
+
+fn parsing_item(c: &mut Criterion) {
+    let fixture =
+        "c29tZXZlcnlsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXNhbnNvbWVvdGhlcmxvbmdsaW5l";
+    c.bench_with_input(
+        BenchmarkId::new("parsing_item", fixture),
+        &fixture,
+        move |bench, &input| {
+            bench.iter(|| Parser::parse_item(input.as_bytes()).unwrap());
+        },
+    );
 }
 
-fn parsing_list(bench: &mut Bencher) {
-    let input = "a, abcdefghigklmnoprst, 123456785686457, 99999999999.999, (), (\"somelongstringvalue\" \"anotherlongstringvalue\";key=:c29tZXZlciBsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXM: 145)";
-    bench.iter(|| Parser::parse_list(input.as_bytes()).unwrap());
+fn parsing_list(c: &mut Criterion) {
+    let fixture = "a, abcdefghigklmnoprst, 123456785686457, 99999999999.999, (), (\"somelongstringvalue\" \"anotherlongstringvalue\";key=:c29tZXZlciBsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXM: 145)";
+    c.bench_with_input(
+        BenchmarkId::new("parsing_list", fixture),
+        &fixture,
+        move |bench, &input| {
+            bench.iter(|| Parser::parse_list(input.as_bytes()).unwrap());
+        },
+    );
 }
 
-fn parsing_dict(bench: &mut Bencher) {
-    let input = "a, dict_key2=abcdefghigklmnoprst, dict_key3=123456785686457, dict_key4=(\"inner-list-member\" :aW5uZXItbGlzdC1tZW1iZXI=:);key=aW5uZXItbGlzdC1wYXJhbWV0ZXJz";
-    bench.iter(|| Parser::parse_dictionary(input.as_bytes()).unwrap());
+fn parsing_dict(c: &mut Criterion) {
+    let fixture = "a, dict_key2=abcdefghigklmnoprst, dict_key3=123456785686457, dict_key4=(\"inner-list-member\" :aW5uZXItbGlzdC1tZW1iZXI=:);key=aW5uZXItbGlzdC1wYXJhbWV0ZXJz";
+    c.bench_with_input(
+        BenchmarkId::new("parsing_dict", fixture),
+        &fixture,
+        move |bench, &input| {
+            bench.iter(|| Parser::parse_dictionary(input.as_bytes()).unwrap());
+        },
+    );
 }
 
-fn serializing_item(bench: &mut Bencher) {
-    let input = "c29tZXZlcnlsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXNhbnNvbWVvdGhlcmxvbmdsaW5l";
-    let parsed_item = Parser::parse_item(input.as_bytes()).unwrap();
-    bench.iter(|| parsed_item.serialize_value().unwrap());
-}
-
-fn serializing_list(bench: &mut Bencher) {
-    let input = "a, abcdefghigklmnoprst, 123456785686457, 99999999999.999, (), (\"somelongstringvalue\" \"anotherlongstringvalue\";key=:c29tZXZlciBsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXM: 145)";
-    let parsed_list = Parser::parse_list(input.as_bytes()).unwrap();
-    bench.iter(|| parsed_list.serialize_value().unwrap());
-}
-
-fn serializing_dict(bench: &mut Bencher) {
-    let input = "a, dict_key2=abcdefghigklmnoprst, dict_key3=123456785686457, dict_key4=(\"inner-list-member\" :aW5uZXItbGlzdC1tZW1iZXI=:);key=aW5uZXItbGlzdC1wYXJhbWV0ZXJz";
-    let parsed_dict = Parser::parse_dictionary(input.as_bytes()).unwrap();
-    bench.iter(|| parsed_dict.serialize_value().unwrap());
-}
-
-benchmark_group!(
-    benches,
-    parsing_item,
-    parsing_list,
-    parsing_dict,
+criterion_group!(
+    serializing,
     serializing_item,
     serializing_list,
     serializing_dict
 );
-benchmark_main!(benches);
+
+fn serializing_item(c: &mut Criterion) {
+    let fixture =
+        "c29tZXZlcnlsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXNhbnNvbWVvdGhlcmxvbmdsaW5l";
+    c.bench_with_input(
+        BenchmarkId::new("serializing_item", fixture),
+        &fixture,
+        move |bench, &input| {
+            let parsed_item = Parser::parse_item(input.as_bytes()).unwrap();
+            bench.iter(|| parsed_item.serialize_value().unwrap());
+        },
+    );
+}
+
+fn serializing_list(c: &mut Criterion) {
+    let fixture = "a, abcdefghigklmnoprst, 123456785686457, 99999999999.999, (), (\"somelongstringvalue\" \"anotherlongstringvalue\";key=:c29tZXZlciBsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXM: 145)";
+    c.bench_with_input(
+        BenchmarkId::new("serializing_list", fixture),
+        &fixture,
+        move |bench, &input| {
+            let parsed_list = Parser::parse_list(input.as_bytes()).unwrap();
+            bench.iter(|| parsed_list.serialize_value().unwrap());
+        },
+    );
+}
+
+fn serializing_dict(c: &mut Criterion) {
+    let fixture = "a, dict_key2=abcdefghigklmnoprst, dict_key3=123456785686457, dict_key4=(\"inner-list-member\" :aW5uZXItbGlzdC1tZW1iZXI=:);key=aW5uZXItbGlzdC1wYXJhbWV0ZXJz";
+    c.bench_with_input(
+        BenchmarkId::new("serializing_dict", fixture),
+        &fixture,
+        move |bench, &input| {
+            let parsed_dict = Parser::parse_dictionary(input.as_bytes()).unwrap();
+            bench.iter(|| parsed_dict.serialize_value().unwrap());
+        },
+    );
+}
