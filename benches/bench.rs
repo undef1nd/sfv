5c419069fec422b77b9b47299f99820649043497
undef1nd <yalyna.ts@gmail.com>
2020-08-04 14:24:48 +0300

    Add api to serialize value incrementally (#70)
    * Add api to serialize incrementally
    
    * Add more benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,9 +2,11 @@
 extern crate criterion;
 
 use criterion::{BenchmarkId, Criterion};
-use sfv::{Parser, SerializeValue};
+use rust_decimal::prelude::FromPrimitive;
+use sfv::{BareItem, Decimal, Parser, SerializeValue};
+use sfv::{RefBareItem, RefDictSerializer, RefItemSerializer, RefListSerializer};
 
-criterion_main!(parsing, serializing);
+criterion_main!(parsing, serializing, ref_serializing);
 
 criterion_group!(parsing, parsing_item, parsing_list, parsing_dict);
 
@@ -84,4 +86,86 @@ fn serializing_dict(c: &mut Criterion) {
             bench.iter(|| parsed_dict.serialize_value().unwrap());
         },
     );
+}
+
+criterion_group!(
+    ref_serializing,
+    serializing_ref_item,
+    serializing_ref_list,
+    serializing_ref_dict
+);
+
+fn serializing_ref_item(c: &mut Criterion) {
+    let fixture =
+        "c29tZXZlcnlsb25nc3RyaW5ndmFsdWVyZXByZXNlbnRlZGFzYnl0ZXNhbnNvbWVvdGhlcmxvbmdsaW5l";
+    c.bench_with_input(
+        BenchmarkId::new("serializing_ref_item", fixture),
+        &fixture,
+        move |bench, &input| {
+            bench.iter(|| {
+                let mut output = String::new();
+                let ser = RefItemSerializer::new(&mut output);
+                ser.bare_item(&RefBareItem::ByteSeq(input.as_bytes()))
+                    .unwrap();
+            });
+        },
+    );
+}
+
+fn serializing_ref_list(c: &mut Criterion) {
+    c.bench_function("serializing_ref_list", move |bench| {
+        bench.iter(|| {
+            let mut output = String::new();
+            let ser = RefListSerializer::new(&mut output);
+            ser.bare_item(&RefBareItem::Token("a"))
+                .unwrap()
+                .bare_item(&RefBareItem::Token("abcdefghigklmnoprst"))
+                .unwrap()
+                .bare_item(&RefBareItem::Integer(123456785686457))
+                .unwrap()
+                .bare_item(&RefBareItem::Decimal(
+                    Decimal::from_f64(99999999999.999).unwrap(),
+                ))
+                .unwrap()
+                .open_inner_list()
+                .close_inner_list()
+                .open_inner_list()
+                .inner_list_bare_item(&RefBareItem::String("somelongstringvalue"))
+                .unwrap()
+                .inner_list_bare_item(&RefBareItem::String("anotherlongstringvalue"))
+                .unwrap()
+                .inner_list_parameter(
+                    "key",
+                    &RefBareItem::ByteSeq("somever longstringvaluerepresentedasbytes".as_bytes()),
+                )
+                .unwrap()
+                .inner_list_bare_item(&RefBareItem::Integer(145))
+                .unwrap()
+                .close_inner_list();
+        });
+    });
+}
+
+fn serializing_ref_dict(c: &mut Criterion) {
+    c.bench_function("serializing_ref_dict", move |bench| {
+        bench.iter(|| {
+            let mut output = String::new();
+            RefDictSerializer::new(&mut output)
+                .bare_item_member("a", &RefBareItem::Boolean(true))
+                .unwrap()
+                .bare_item_member("dict_key2", &RefBareItem::Token("abcdefghigklmnoprst"))
+                .unwrap()
+                .bare_item_member("dict_key3", &RefBareItem::Integer(123456785686457))
+                .unwrap()
+                .open_inner_list("dict_key4")
+                .unwrap()
+                .inner_list_bare_item(&RefBareItem::String("inner-list-member"))
+                .unwrap()
+                .inner_list_bare_item(&RefBareItem::ByteSeq("inner-list-member".as_bytes()))
+                .unwrap()
+                .close_inner_list()
+                .parameter("key", &RefBareItem::Token("aW5uZXItbGlzdC1wYXJhbWV0ZXJz"))
+                .unwrap();
+        });
+    });
 }
