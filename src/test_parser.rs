b9c6d5957acb2f0808cb7c3e9242b3ab5a2e2c19
Andrew Paseltiner <apaseltiner@google.com>
2025-03-31 08:40:26 -0400

    Add tests for invalid date / display string start

--- src/test_parser.rs
+++ src/test_parser.rs
@@ -873,6 +873,13 @@ fn parse_date() -> Result<(), Error> {
 }
 
 #[test]
+#[should_panic]
+#[cfg(debug_assertions)]
+fn parse_invalid_date_start() {
+    let _ = Parser::new("7").parse_date();
+}
+
+#[test]
 #[cfg(feature = "parsed-types")]
 fn parse_display_string() -> Result<(), Error> {
     let input = r#"%"This is intended for display to %c3%bcsers.""#;
@@ -890,4 +897,11 @@ fn parse_display_string() -> Result<(), Error> {
     );
 
     Ok(())
+}
+
+#[test]
+#[should_panic]
+#[cfg(debug_assertions)]
+fn parse_invalid_display_string_start() {
+    let _ = Parser::new(r#"""#).parse_display_string();
 }
