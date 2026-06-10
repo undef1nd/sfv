bd70648727b2d054fa5533f04778a779d1429a97
Andrew Paseltiner <apaseltiner@google.com>
2026-06-10 10:18:57 -0400

    Fix documentation references

--- src/lib.rs
+++ src/lib.rs
@@ -139,8 +139,8 @@ assert_eq!(
 # Crate features
 
 - `parsed-types` (enabled by default) -- When enabled, exposes fully owned types
-  `Item`, `Dictionary`, `List`, and their components, which can be obtained from
-  `Parser::parse_item`, etc. These types are implemented using the
+  [`Item`], [`Dictionary`], [`List`], and their components, which can be obtained from
+  [`Parser::parse`], etc. These types are implemented using the
   [`indexmap`](https://crates.io/crates/indexmap) crate, so disabling this
   feature can avoid that dependency if parsing using a visitor
   ([`Parser::parse_item_with_visitor`], etc.) is sufficient.
